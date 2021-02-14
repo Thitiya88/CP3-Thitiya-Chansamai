68361666436041ba2e3793c51aefc9ff582a4ae5
Buddy-Coder88 <krataigrumble@gmail.com>
2021-02-14 18:16:24 +0700

    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร
    สร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชา

---
+++ Exercise4_Thitiya_C.py
@@ -0,0 +1,9 @@
+FE_Score = 80.5
+GB_Score = 77.8
+ICS_Score = 90.0
+CP_Score = 85.0
+print("--- Your Score ---")
+print('Foundation English :',FE_Score)
+print('General Business :',GB_Score)
+print('Introduction to Computer System :',ICS_Score)
+print('Computer Programming :',CP_Score)
